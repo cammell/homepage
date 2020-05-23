f9db3973359700b7676376e504aa70f29ddb259b
Cammell <cammell@gmail.com>
2020-05-23 16:54:00 +0200

    used inner html in main.js to add date in navbar in all files

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const name="Pawel",
         age=44;
 
 console.log(`Hello there, my name is ${name} and im ${age}`);
+
+const date=new Date();
+document.querySelector('.navigation-header__date--js').innerHTML=date;
