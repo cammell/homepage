29c34e3d05240da4cce85cccd5ddfb8a59f44a1d
Cammell <cammell@gmail.com>
2020-07-16 21:29:04 +0200

    added new function taking name and age as arguments

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ console.log(`Hello there, my name is ${name} and im ${age}`);
 
 const date=new Date();
 document.querySelector('.navigation-header__date--js').innerHTML=date;
+
+
+function printNameAge(name, age) {
+        //console.log('Hello from function printNameAge. My name is ${name} and im ${age}');
+        document.querySelector('.article--js').innerHTML=`<p>Hello from function printNameAge. My name is ${name} and im ${age}</p>`
+}
